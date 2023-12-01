b9a07f420fb8478a83a250aa1a53391a89719c54
DPeeta <dayronalejandrocruzpita@gmail.com>
2023-11-30 20:21:31 -0500

    tarea3.1.1

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -5,15 +5,22 @@ from odoo import models, fields, api
 class Course(models.Model):
     _name = 'openacademy.course'
     _description = "OpenAcademy Courses"
-
+    
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
+
+    @api.onchange('description', 'name')
+    def _onchange_description(self):
+        for r in self:
+            if not r.description and r.name:  # Si description está vacío y name tiene un valor
+                r.description = r.name  # Tomar el mismo valor que name
 
     responsible_id = fields.Many2one('res.users',
         ondelete='set null', string="Responsible", index=True)
 
 class Session(models.Model):
     _name = 'openacademy.session'
+    _description = "OpenAcademy Sessions"
 
     name = fields.Char(required=True)
     start_date = fields.Date()
