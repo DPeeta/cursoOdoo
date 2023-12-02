4af735c717de1ce6d63f763985ec0b8a5b5a9a83
DPeeta <dayronalejandrocruzpita@gmail.com>
2023-12-01 21:29:57 -0500

    IntentoTarea3.3NoFuncionaNoDaError

--- openacademy/wizard/wizard.py
+++ openacademy/wizard/wizard.py
@@ -3,7 +3,7 @@
 from odoo import models, fields, api
 from datetime import date
 
-
+#  wizard suscrivir varios usuarios a varias sesiones 
 class Wizard(models.TransientModel):
     _name = 'openacademy.wizard'
     _description = "Wizard: Quick Registration of Attendees to Sessions"
@@ -18,4 +18,19 @@ class Wizard(models.TransientModel):
                 session.attendee_ids |= self.attendee_ids
         return {}
 
+#  todas las sesiones con duración A (en días) la reemplace por le valor de B
+class Wizard(models.TransientModel):
+    _name = 'openacademy.wizardrempduration'
+    _description = "Wizard: Replace in sessions with duration A with B "
 
+    sessions_id = fields.Many2many('openacademy.session',
+        string="Sessions")
+    duration = fields.Float(digits=(6, 2), help="Value of duration in days")
+    duration_find = fields.Float(digits=(6, 2), help="Value of duration in days to find", string="Sessions with this duration")
+    duration_replace = fields.Float(digits=(6, 2), help="New value of duration in days", string="Will be replaced for this one")
+
+    def replace(self):
+        for session in self.sessions_id:
+            if (session.duration  and session.duration == self.duration_find):
+                session.duration == self.duration_replace
+        return {}
